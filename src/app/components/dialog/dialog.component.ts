c87b94b7a4f5e6cf362beba08aef5ae53989d3db
Deepanshukesarwani <kesarwanideepanshu428@gmail.com>
2024-07-19 01:28:26 +0530

    added user dialog to add user ui part

--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { DialogModule } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { FormsModule } from '@angular/forms';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
 @Component({
   selector: 'app-dialog',
   standalone: true,
-  imports: [DialogModule, ButtonModule],
+  imports: [DialogModule, ButtonModule,CardModule,FloatLabelModule, FormsModule, InputGroupModule, InputGroupAddonModule, InputTextModule],
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.css'
 })
 export class DialogComponent {
   visible: boolean = false;
-
+  value: string | undefined;
   showDialog() {
       this.visible = true;
   }
